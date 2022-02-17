b49b4cedbc1c84a3f21c895ad18b686b34fed7f9
Diwakar <vinaydiwakar10@gmail.com>
2022-02-17 14:14:26 +0530

    Pascals triangle implemented in cpp

--- 0-Module_Basics/PascalsTriangle.cpp
+++ 0-Module_Basics/PascalsTriangle.cpp
@@ -1,30 +1,44 @@
 #include<iostream>
 #include<vector>
+#include<cstdio>
 
-// std::vector<std::vector<int>>
-int generate(int numRows) {
-        for (int i = 1; i <= numRows; i++)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                std::cout<<j;
+std::vector<std::vector<int>> generate(int numRows) {
+    std::vector<std::vector<int>> ans = {};
+    
+    for(int i=0;i<numRows;i++){
+        std::vector<int> temp = {};
+        for(int j=0;j<=i;j++)            
+            if(j==0 || j==i){
+                temp.push_back(1);
             }
-            std::cout<<std::endl;
-        }
-    return 0;     
-
+            else{
+                temp.push_back(ans[i-1][j]+ans[i-1][j-1]);
+            }
+            ans.push_back(temp);
+    }
+    std::cout<<std::endl;
+    return ans;    
     }
 
 int main(int argc, char const *argv[])
 {
-    auto ans = generate(5);
-
-    // for(auto rows : ans)
-    // {
-    //     for(auto nums : rows)
-    //     {
-    //         std::cout<<nums<<std::endl;   
-    //     }   
-    // }
+    int n = 0;
+    int k=1;
+    std::cin>>n;
+    auto ans = generate(n);
+    for(auto rows : ans)
+    {
+        int x=n-k;
+        while(x!=0){
+            printf(" ");
+            x--;
+        }
+        for(auto nums : rows)
+        {
+            std::cout<<nums<<" ";   
+        }   
+        std::cout<<std::endl;
+        k++;
+    }
     return 0;
 }
